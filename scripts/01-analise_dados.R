5ecbd4747455777774e42bc117a941138493009f
saramortara <saramortara@gmail.com>
2020-09-23 20:15:43 -0300

    esqueleto do script

---
+++ scripts/01-analise_dados.R
@@ -0,0 +1,13 @@
+# Script para fazer download dos dados e gerar tabelas e figuras
+
+# Bibliotecas necessárias
+library(palmerpenguins)
+library(ggplot2)
+
+# Carregando os dados ----------------------------------------------------------
+head(penguins)
+
+# Criando tabela ---------------------------------------------------------------
+# exporta a tabela formatada
+
+# Criando a figura -------------------------------------------------------------
