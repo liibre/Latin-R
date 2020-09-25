4d56c1c4a98f2a1a5c883dc646818bb6dd9f31ed
saramortara <saramortara@gmail.com>
2020-09-25 17:33:34 -0300

    mapa bonus WIP

---
+++ scripts/02-mapa.R
@@ -0,0 +1,73 @@
+
+# Script para fazer um- bônus :)
+
+# Bibliotecas necessárias
+library(palmerpenguins)
+library(ggplot2)
+library(sf)
+library(ggspatial)
+library(cowplot)
+library(magick)
+
+# Carregando os dados ----------------------------------------------------------
+## Dados dos penguins
+head(penguins)
+
+## Download do Shapefile da Antartica direto do GADM
+# cod <- "ATA" # Codigo iso3 do pais Antarctica
+# level <- 0 # para download do maior nivel do pais (sem subdivisoes)
+# file <- paste0(cod, "_", level, "_sf.rds")
+# destfile <- paste0("./data/GADM/", file)
+# # Faz o download e salva na pasta data/GADM
+# url <- paste0("https://biogeo.ucdavis.edu/data/gadm3.6/Rsf/gadm36_", file)
+# download.file(url, destfile = destfile)
+# Carregando o objeto para o R
+ata <- readRDS("data/GADM/ATA_0_sf.rds")
+
+# Coordenadas das ilhas
+coord <- data.frame(Island = c('Biscoe', 'Torgersen', 'Dream'),
+                    lon = c(-63.766667, -64.066667, -64.216667),
+                    lat = c(-64.800000, -64.766667, -64.716667))
+
+# Caminho para figuras dos pinguins
+adelie <- "figs/adelie.png"
+chinstrap <- "figs/chinstrap.png"
+gentoo <- "figs/gentoo.png"
+
+# Criando o mapa ---------------------------------------------------------------
+penguin_map <- ggplot() +
+  geom_sf(data = ata) +
+#  coord_sf(crs = polar_proj) +
+  #coord_sf(xlim = c(-68, -55), ylim = c(-66, -60)) +
+  coord_sf(xlim = c(-66, -60), ylim = c(-65, -63)) +
+  geom_segment(data = coord, aes(x = lon,  xend = lon - c(0, 0.7, 1),
+                                 y = lat, yend = lat + c(1, 0.7, 0))) +
+  geom_point(data = coord, aes(x = lon, y = lat, shape = Island),
+             size = 2) +
+  annotation_scale(location = "bl", width_hint = 0.5,
+                   pad_x = unit(0.1, "in"), pad_y = unit(0.2, "in")) +
+  annotation_north_arrow(location = "bl", which_north = "true",
+                         pad_x = unit(6, "in"), pad_y = unit(0.5, "in"),
+                         style = north_arrow_fancy_orienteering) +
+  xlab("Longitude") + ylab("Latitude") +
+  theme_minimal()
+
+penguin_map
+
+# Adicionando figuras dos pinguins nos mapas -----------------------------------
+all_map <- ggdraw() +
+  draw_plot(penguin_map) +
+  draw_image(adelie,  x = -0.12, y = 0.1, scale = .1) +
+  draw_image(gentoo,  x = -0.12, y = 0.01, scale = .1) +
+  draw_image(adelie,  x = -0.25, y = 0, scale = .1) +
+  draw_image(adelie,  x = -0.33, y = -0.16, scale = .1) +
+  draw_image(chinstrap,  x = -0.33, y = -0.25, scale = .1)
+
+all_map # so fica bom no plot zoom
+
+# Salvando o mapa # fail
+png("figs/mapa_pinguins.png", res = 300, width = 2000, height = 1000)
+all_map
+dev.off()
+
+
